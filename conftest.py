364b4ecaa65e53981a71eb29325251e73129cba6
yoneda hayato <yhayato1320@gmail.com>
2018-09-25 11:55:59 +0900

    added new file

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import inspect
+import logging
+import os
+import pytest
+
+logging.basicConfig(filename='pytest.log',level=logging.DEBUG)
+
+@pytest.fixture(scope='session', autouse=True)
+def scope_session():
+    print("setup before session")
+    yield
+    print("teardown after session")
+
+@pytest.fixture(scope='module', autouse=True)
+def scope_module():
+    print("    setup before module")
+    yield
+    print("    teardown after module")
+
+@pytest.fixture(scope='class', autouse=True)
+def scope_class():
+    print("        setup before class")
+    yield
+    print("        teardown after class")
+
+def _log():
+    def_name = inspect.currentframe().f_back.f_code.co_name
+    logging.debug('%s:%s' % (os.getpid(), def_name))
+
+@pytest.fixture(scope='module', autouse=True)
+def module():
+    _log()
+
+@pytest.fixture(scope='function', autouse=True)
+def function():
+    _log()
